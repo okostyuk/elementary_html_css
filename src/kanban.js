e12fb2bcc0da2ff3d364404fef08ab09688c3665
Oleg Kostiuk <kostyuk.oleg@gmail.com>
2020-01-31 20:45:05 +0200

    Drag and Drop task update

--- src/kanban.js
+++ src/kanban.js
@@ -1,42 +1,21 @@
 // Fiddle url: https://jsfiddle.net/okostyuk/u5ch2md7/
 
 let currentDroppable;
+let placeholder;
 
 let tasks = document.getElementsByClassName("task");
 let headers = document.getElementsByClassName("header");
 
-function hidePreview(dragStartEvent) {
-    var copyItem = this.cloneNode(true);
-    copyItem.style.display = "none";
-    dragStartEvent.dataTransfer.setDragImage(copyItem, 0, 0);
-}
-
-function initStaticPos(dragStartEvent, task) {
-    task.shiftX = dragStartEvent.clientX - task.getBoundingClientRect().left;
-    task.shiftY = dragStartEvent.clientY - task.getBoundingClientRect().top;
-    task.style.left = dragStartEvent.pageX-task.shiftX +'px';
-    task.style.top = dragStartEvent.pageY-task.shiftY + 'px';
-}
-
-function updateStaticPos(event, task) {
-    task.style.left = event.pageX-task.shiftX +'px';
-    task.style.top = event.pageY-task.shiftY + 'px';
-}
-
-const draggedClass = "draged";
-const expandClass = "expandTask";
+const classExpand = "expandTask";
 const classCollapse = "collapseTask";
 
 function createPlaceholder() {
-    return document.createElement("div");
-}
-
-function createDragPreview(task) {
-    //make preview and move it off the screen
-    let preview = task.cloneNode(true);
-    preview.style.position = 'absolute';
-    preview.style.left = '-500px';
-    return preview;
+    let placeholder = document.createElement("div");
+    placeholder.className = classExpand;
+    placeholder.ondragover = function (e) {
+        e.preventDefault();
+    };
+    return placeholder;
 }
 
 for (let task of tasks) {
@@ -46,50 +25,48 @@ for (let task of tasks) {
     task.ondragstart = function(event) {
         console.log("task onDragStart()");
         currentDroppable = task;
-        task.dragPreview = createDragPreview(task);
-        document.body.appendChild(task.dragPreview);
-        event.dataTransfer.setDragImage(task.dragPreview, 0, 0);
-        task.classList.add(draggedClass);
+        event.dataTransfer.effectAllowed='move';
     };
 
     task.ondragend = function(event) {
         console.log("task ondragend "+ event.target.id);
-        task.classList.remove(draggedClass);
-        task.dragPreview.remove();
+
+        if (placeholder !== undefined) {
+            placeholder.parentNode.replaceChild(currentDroppable, placeholder);
+            task.style.display = '';
+            placeholder = undefined;
+        }
         currentDroppable = null;
+
+        let placeholders = document.getElementsByClassName(classCollapse);
+        for (let item of placeholders) {
+            item.remove();
+        }
     };
 
     //  *** drag over this item events handling   *** //
     task.ondragenter = function (event) {
         console.log("task " + task.id + ": ondragenter");
         if (task === currentDroppable) {
+            placeholder = createPlaceholder();
+            placeholder.className = "taskPlaceholder";
+            task.after(placeholder);
+            task.style.display = 'none';
             //do something?
         } else {
             //hide previous placeholder and show new placeholder
-            task.dropaplePlaceholder = createPlaceholder();
-            task.dropaplePlaceholder.classList.add(expandClass);
+            if (placeholder !== undefined) {
+                placeholder.className = classCollapse;
+            }
+            placeholder = createPlaceholder();
+            task.dropaplePlaceholder = placeholder;
+            task.dropaplePlaceholder.classList.add(classExpand);
             task.after(task.dropaplePlaceholder);
         }
     };
 
     task.ondragover = function (event) {
         event.preventDefault(); //ugly hack to make this Element dropable
-    };
-
-    task.ondragleave = function (event) {
-        console.log("task " + task.id + ": ondragleave ph=" + task.dropaplePlaceholder);
-        if (task.dropaplePlaceholder !== undefined) {
-            task.dropaplePlaceholder.classList.replace(expandClass, classCollapse);
-            task.dropaplePlaceholder = undefined;
-        }
-    };
-
-    task.ondrop = function () {
-        console.log("task " + task.id + ": ondrop ph=" + task.dropaplePlaceholder);
-        if (task.dropaplePlaceholder !== undefined) {
-            task.dropaplePlaceholder.remove();
-            task.after(currentDroppable);
-        }
     };
 }
 
@@ -102,25 +79,12 @@ for (let header of headers) {
     header.ondragenter = function (event) {
         console.log("header: ondragenter");
 
-        header.dropaplePlaceholder = createPlaceholder();
-        header.dropaplePlaceholder.classList.add(expandClass);
-        header.after(header.dropaplePlaceholder);
-    };
-
-    header.ondragleave = function (event) {
-        console.log("header: ondragleave ph=" + header.dropaplePlaceholder);
-        if (header.dropaplePlaceholder !== undefined) {
-            header.dropaplePlaceholder.classList.replace(expandClass, classCollapse);
-            header.dropaplePlaceholder = undefined;
+        if (placeholder !== undefined) {
+            placeholder.className = classCollapse;
         }
-    };
 
-    header.ondrop = function () {
-        console.log("header: ondrop");
-        if (header.dropaplePlaceholder !== undefined) {
-            header.dropaplePlaceholder.remove();
-            header.after(currentDroppable);
-        }
-    }
+        placeholder = createPlaceholder();
+        header.after(placeholder);
+    };
 }
 
